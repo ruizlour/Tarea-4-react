61a2f303570842ad90aabd603ec23957a7102ea6
María Lourdes <35180135+Maria-Lour@users.noreply.github.com>
2023-07-17 20:00:41 -0300

    Actualizo el fetch del poster al azar
    Actualizo la manera que hago el fetch para mostrar el poster al azar

--- fetchposter.js
+++ fetchposter.js
@@ -1,51 +1,15 @@
-postersPeliculas = [];
-url1 = "https://www.omdbapi.com/?apikey=8645057e&t=Back+to+the+future";
-url2 = "https://www.omdbapi.com/?apikey=8645057e&t=Star+Wars";
-url3 = "https://www.omdbapi.com/?apikey=8645057e&t=Revenge+of+the+Nerds";
-url4 = "https://www.omdbapi.com/?apikey=8645057e&t=Police+Academy";
-url5 = "https://www.omdbapi.com/?apikey=8645057e&t=Avatar";
+const postersPeliculas = [
+    "https://www.omdbapi.com/?apikey=8645057e&t=Back+to+the+future",
+    "https://www.omdbapi.com/?apikey=8645057e&t=Star+Wars",
+    "https://www.omdbapi.com/?apikey=8645057e&t=Revenge+of+the+Nerds",
+    "https://www.omdbapi.com/?apikey=8645057e&t=Police+Academy",
+    "https://www.omdbapi.com/?apikey=8645057e&t=Avatar"
+];
 
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
 
-numeroRandom = randomNumber(1, 5)
-let enlace = url.numeroRandom;
-
-fetch(enlace)
+fetch(postersPeliculas[Math.floor(Math.random() * 5)])
     .then(response => response.json())
     .then(data => {
-        postersPeliculas = data.Poster;
-        document.querySelector("#poster img").src = postersPeliculas;
+        const posterPelicula = data.Poster;
+        document.querySelector("#poster img").src = posterPelicula;
     });
-/*
-fetch('https://www.omdbapi.com/?apikey=8645057e&t=Top+Gun')
-    .then(response => response.json())
-    .then(data => {
-        postersPeliculas2 = data.Poster;
-    });
-
-fetch('https://www.omdbapi.com/?apikey=8645057e&t=Star+Wars')
-    .then(response => response.json())
-    .then(data => {
-        postersPeliculas3 = data.Poster;
-    });
-
-fetch('https://www.omdbapi.com/?apikey=8645057e&t=Revenge+of+the+Nerds')
-    .then(response => response.json())
-    .then(data => {
-        postersPeliculas4 = data.Poster;
-    });
-fetch('https://www.omdbapi.com/?apikey=8645057e&t=Police+Academy')
-    .then(response => response.json())
-    .then(data => {
-        postersPeliculas5 = data.Poster;
-    });
-fetch('https://www.omdbapi.com/?apikey=8645057e&t=Avatar')
-    .then(response => response.json())
-    .then(data => {
-        postersPeliculas6 = data.Poster;
-    }); 
-
-    document.querySelector("#poster img").src = postersPeliculas;
-    */
